5dc5961f264ceaefd0f1bd83e7c3c843dddc0644
gfranco7 <deepagmf710@gmail.com>
2025-06-24 16:20:40 -0500

    Fqs Bot v.1

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
+import os
 import pandas as pd 
 import faiss
 import pickle
 from sentence_transformers import SentenceTransformer, util
 
-csv_file = pd.read_csv('fqs.csv')
-# print(csv_file.head())
 
-model = SentenceTransformer("All-MiniLM-L6-v2")
-# index = faiss.read_index("faq_index.faiss")
+CSV_PATH = "fqs.csv"
+INDEX_PATH = "faq_index.faiss"
+DATA_PATH = "faq_data.pkl"
+
+if not os.path.exists(CSV_PATH):
+    print(f"No se encontró el archivo {CSV_PATH}")
+
+
+df = pd.read_csv(CSV_PATH)
+questions = df["question"].tolist()
+answers = df["answer"].tolist()
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+index = faiss.read_index(INDEX_PATH)
+
+print("Bienvenido a Amazun\n")
+user_input = input("Input your question: ")
+
+embedding = model.encode([user_input])
+
+D, I= index.search(embedding, k=1)
+
+idx = I[0][0]
+distancia = D[0][0]
+
+if distancia < 1.0:
+    print("||")
+    print(f"Te efieres a: {questions[idx]}")
+    print(f"Respuesta: {answers[idx]}")
+else:
+    print("||")
+    print("No se encontró una respuesta adecuada")
 
